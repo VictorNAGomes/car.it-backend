3ddd815aebb32b5c671ea28dac99040b24b5fe41
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-09-06 13:59:20 -0300

    feat: setting up the main file structure to the project

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+})
+
+export default knex
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('teste')
+const express = require('express')
+const router = require('./routes')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(router)
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('Server is running on http://localhost:' + process.env.SERVER_PORT)
+})
