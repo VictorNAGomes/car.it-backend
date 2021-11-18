206811417b7da97a3ff8986982948233541c05ea
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-11-17 21:29:31 -0300

    fix: change upload path folder

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,10 +3,10 @@ const path = require('path')
 const crypto = require('crypto')
 
 module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'images'),
+  dest: path.resolve(__dirname, '..', '..', 'public', 'uploads'),
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'images'))
+      cb(null, path.resolve(__dirname, '..', '..', 'public', 'uploads'))
     },
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
