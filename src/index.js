13f1746a51c7a0bdca02cdb103ea2c3cc1a13ff2
MarcosFajoli <marcos.almeida83@etec.sp.gov.br>
2021-11-24 19:32:42 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = require('./routes')
+const cors = require('cors')
 const app = express()
 
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors({
+  credentials: false,
+  origin: '*'
+}))
 
 app.use(router)
 
