9656b659eaa2d714638a032928aafb4bddf80913
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-11-24 23:22:52 -0300

    feat: add cors library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = require('./routes')
 const app = express()
+const cors = require('cors')
 
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(
+  cors({
+    credentials: false,
+    origin: '*'
+  })
+)
 
 app.use(router)
 
